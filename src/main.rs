ab3ac4bca256b448e4552f481bf2607800144a19
Ptrskay3 <leeh123peter@gmail.com>
2021-11-11 21:46:50 +0100

    progress with playing around

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-// use colored::Colorize;
-// use git2::{BlameOptions, Repository};
+use colored::Colorize;
+use git2::Repository;
 use ignore::DirEntry;
-// use jwalk::{Parallelism, WalkDir};
+use indicatif::ProgressIterator;
 use lazy_static::lazy_static;
-// use rayon::prelude::*;
 use regex::{Regex, RegexBuilder};
 use std::collections::HashMap;
-// use std::ffi::OsStr;
+use std::path::Path;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 
@@ -18,9 +17,9 @@ lazy_static! {
 }
 
 fn count_todos_with_ignore() {
-    // let mut blame_options = BlameOptions::new();
-    let path = "/home/leehpeter/projects/PySprint/PySprint";
-    // let repo = Repository::open(path).unwrap();
+    let mut buffer = String::new();
+    let path = "/home/leehpeter/";
+    let repo = Repository::open(path).unwrap();
     let blame_map: Arc<Mutex<HashMap<String, usize>>> = Arc::new(Mutex::new(HashMap::new()));
     let count = AtomicUsize::new(0);
     let _walker = ignore::WalkBuilder::new(path).build_parallel().run(|| {
@@ -30,38 +29,31 @@ fn count_todos_with_ignore() {
             ignore::WalkState::Continue
         })
     });
-    println!("{:#?}", blame_map.lock().unwrap());
-    // println!("{:#?}", blame_map.lock().unwrap().len());
-    println!("{:?}", count.load(Ordering::Relaxed));
+    let blame_map = Arc::try_unwrap(blame_map).unwrap();
+    for (key, value) in blame_map
+        .into_inner()
+        .unwrap()
+        .into_iter()
+        .progress()
+        .nth(1)
+    {
+        let rel_path = &key.strip_prefix(path).unwrap();
+        println!("we are looking at {:?} with #{}", rel_path, value);
+        let sig = repo.blame_file(Path::new(rel_path), None).unwrap();
+        buffer.push_str(&format!(
+            "it was {:?}\n",
+            sig.get_line(value)
+                .unwrap()
+                .final_signature()
+                .name()
+                .unwrap()
+        ));
+    }
+    println!("{}", buffer);
 }
 
-// fn count_todos() -> usize {
-//     let count = std::sync::atomic::AtomicUsize::new(0);
-//     WalkDir::new("/home/leehpeter/mozaweb/js")
-//         .parallelism(Parallelism::RayonNewPool(4))
-//         .into_iter()
-//         .par_bridge()
-//         .for_each(|dir_entry_result| {
-//             let dir_entry = dir_entry_result.unwrap();
-//             if dir_entry.file_type().is_file()
-//             // && (dir_entry.path().extension().and_then(OsStr::to_str) == Some("js"))
-//             {
-//                 let path = dir_entry.path();
-//                 let text = std::fs::read_to_string(&path).unwrap_or_default();
-//                 for (i, line) in text.lines().enumerate() {
-//                     if RE.find(line).is_some() {
-//                         count.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-//                         // println!("{}:{}:{}", &path.display(), i + 1, 0);
-//                         // println!("{}", &line.red());
-//                     }
-//                 }
-//             }
-//         });
-//     count.load(std::sync::atomic::Ordering::Relaxed)
-// }
-
-fn process_file<'a>(
-    entry: &'a DirEntry,
+fn process_file(
+    entry: &DirEntry,
     count: &AtomicUsize,
     blame_map: &Arc<Mutex<HashMap<String, usize>>>,
 ) {
@@ -69,12 +61,14 @@ fn process_file<'a>(
     let text = std::fs::read_to_string(&path).unwrap_or_default();
     for (i, line) in text.lines().enumerate() {
         if RE.find(line).is_some() {
+            println!("found at {:?}", line);
+            println!("at path {:?}", path);
             count.fetch_add(1, Ordering::SeqCst);
             blame_map
                 .lock()
                 .unwrap()
                 .insert(path.to_str().unwrap().to_owned(), i + 1);
-            println!("{}:{}:{}", &path.display(), i + 1, 0);
+            // println!("{}:{}:{}", &path.display(), i + 1, 0);
             // println!("{}", &line.red());
         }
     }
@@ -82,17 +76,13 @@ fn process_file<'a>(
 
 fn main() {
     println!(
-        r#"_/\\\\\\\\\\\\\\\_______________________________________________________        
-\/\\\///////////________________________________________________________       
- \/\\\______________/\\\_________________________________________________      
-  \/\\\\\\\\\\\_____\///___/\\\____/\\\____/\\\\\__/\\\\\_______/\\\\\\\\_     
-   \/\\\///////_______/\\\_\///\\\/\\\/___/\\\///\\\\\///\\\___/\\\/////\\\    
-    \/\\\_____________\/\\\___\///\\\/____\/\\\_\//\\\__\/\\\__/\\\\\\\\\\\_   
-     \/\\\_____________\/\\\____/\\\/\\\___\/\\\__\/\\\__\/\\\_\//\\///////__  
-      \/\\\_____________\/\\\__/\\\/\///\\\_\/\\\__\/\\\__\/\\\__\//\\\\\\\\\\ 
-       \///______________\///__\///____\///__\///___\///___\///____\//////////_"#
+        r#"  ______ _                     
+ |  ____(_)                    
+ | |__   ___  ___ __ ___   ___ 
+ |  __| | \ \/ / '_ ` _ \ / _ \
+ | |    | |>  <| | | | | |  __/
+ |_|    |_/_/\_\_| |_| |_|\___|"#
     );
 
-    // println!("Fixme count {:?}", count_todos());
     count_todos_with_ignore();
 }
